afa2a9be0f7203b16987289128bfe84d23868990
bohong <977106024@qq.com>
2020-06-19 22:23:16 +0800

    输入票面

--- change.c
+++ change.c
@@ -3,11 +3,17 @@
 int main()
  {
  	int price = 0;
+ 	int amount = 100; 
  	
  	printf("请输入金额（元）：");
  	scanf("%d",&price);
 	 
-	int change = 100 - price;
+	printf("请输入票面:");
+	scanf("%d",&amount);
+	
+	int change = amount - price;  
 	 
-	printf("找您%d元。\n",change); 
+	printf("找您%d元。\n",change);
+	
+	return 0; 
  }
